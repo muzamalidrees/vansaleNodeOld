eac1458e042d58aeeae3a1179ade97d4b1bf6724
muzamal <muzzammal.idrees@gmail.com>
2019-04-27 17:56:36 +0500

    pwa-initialized

---
+++ build/precache-manifest.3c34c17cf79cbe8197811b56da26a7e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7f6fe9a4c14219039dc4",
+    "url": "/static/js/main.0f60838b.chunk.js"
+  },
+  {
+    "revision": "246ba2370813524d1792",
+    "url": "/static/js/2.db7d56a7.chunk.js"
+  },
+  {
+    "revision": "7f6fe9a4c14219039dc4",
+    "url": "/static/css/main.c01c1a60.chunk.css"
+  },
+  {
+    "revision": "246ba2370813524d1792",
+    "url": "/static/css/2.64dd9fe2.chunk.css"
+  },
+  {
+    "revision": "86efc559baebd7e5dc6543b67e2ddfbb",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2005d0093df81b4d52f46d127965f927.js"
+  "/precache-manifest.3c34c17cf79cbe8197811b56da26a7e6.js"
 );
 
 workbox.clientsClaim();
