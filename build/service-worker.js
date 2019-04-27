8db418ab4b6b40e5661e92b56d7a76fa24f4a599
muzamal <muzzammal.idrees@gmail.com>
2019-04-27 18:03:16 +0500

    pWA working

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c34c17cf79cbe8197811b56da26a7e6.js"
+  "/precache-manifest.22a47a85dc6bee5368eb472d27086e93.js"
 );
 
 workbox.clientsClaim();
