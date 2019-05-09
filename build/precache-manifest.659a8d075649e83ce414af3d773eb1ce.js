85657833e29edf3ad278ceae2b199052c64ddf46
muzamal <muzzammal.idrees@gmail.com>
2019-05-09 13:51:42 +0500

    bug in securedsalesorreturn area fixed

---
+++ build/precache-manifest.659a8d075649e83ce414af3d773eb1ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6448a908ae3fb682d401",
+    "url": "/static/js/main.68a39a2f.chunk.js"
+  },
+  {
+    "revision": "9a766036b94dcdb3537e",
+    "url": "/static/js/2.1e10f574.chunk.js"
+  },
+  {
+    "revision": "6448a908ae3fb682d401",
+    "url": "/static/css/main.b0757e30.chunk.css"
+  },
+  {
+    "revision": "9a766036b94dcdb3537e",
+    "url": "/static/css/2.64dd9fe2.chunk.css"
+  },
+  {
+    "revision": "941e711c488ca5aac4441f944af12526",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6caf7bba6498157a19a9a53676750830.js"
+  "/precache-manifest.659a8d075649e83ce414af3d773eb1ce.js"
 );
 
 workbox.clientsClaim();
